7f3093cfbf5ea39964e1aa049db179f42fa47aca
Ophelia Indigo <ophelia.indigo@outlook.com>
2024-08-10 18:52:19 +1000

    Added mkdir functionality for outPath

--- src/main/scala/com/silhouette/App.scala
+++ src/main/scala/com/silhouette/App.scala
@@ -1,12 +1,38 @@
 package com.silhouette
 
-/**
- * @author ${user.name}
- */
-  
-def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+import com.silhouette.project.Project
+import com.silhouette.project.BasicProjectTemplate
+import com.silhouette.project.ProjectTemplateLocalPath
+
+/** @author
+  *   ${user.name}
+  */
+
+def foo(x: Array[String]) = x.foldLeft("")((a, b) => a + b)
 
 @main def potato() =
-  println( "Hello World!" )
-  println("concat arguments = " + foo(Array("potato")))
+    val projectBasePath = ProjectTemplateLocalPath(
+      os.Path(
+        "/home/ophelia/silhouette/ProjectManager/project-silhouette/template"
+      )
+    )
 
+    val projectProjPath = ProjectTemplateLocalPath(
+      projectBasePath.path / "testTemplate" / "proj"
+    )
+    val projectNotesPath = ProjectTemplateLocalPath(
+      projectBasePath.path / "testTemplate" / "notes"
+    )
+    val projectOutPath = ProjectTemplateLocalPath(
+      projectBasePath.path / "out"
+    )
+
+    val testProjTempl = BasicProjectTemplate(
+      "Test Template",
+      "Ophelia",
+      List(
+        projectProjPath,
+        projectNotesPath
+      )
+    )
+    Project("Potato", "Ophelia", testProjTempl, projectOutPath).init()
--- src/main/scala/com/silhouette/project/ProjectTemplate.scala
+++ src/main/scala/com/silhouette/project/ProjectTemplate.scala
@@ -1,9 +1,12 @@
 package com.silhouette.project
 
-import java.nio.file.Path
-
-sealed trait ProjectTemplatePath
-case class ProjectTemplateLocalPath(path: os.Path) extends ProjectTemplatePath
+sealed trait ProjectTemplatePath {
+    def createIfNotExists(): Unit
+}
+case class ProjectTemplateLocalPath(path: os.Path) extends ProjectTemplatePath {
+    override def createIfNotExists(): Unit = 
+        os.makeDir.all(path)
+}
 
 trait ProjectTemplate {
     val name: String // Project Template Name
@@ -15,11 +18,12 @@ trait ProjectTemplate {
 class BasicProjectTemplate(
     val name: String,
     val author: String,
-    val dirs: List[ProjectTemplateLocalPath]
+    val dirs: List[ProjectTemplateLocalPath],
 ) extends ProjectTemplate {
     override def initProjectFolders(
         newDir: ProjectTemplatePath
     ): Unit = {
+        newDir.createIfNotExists()
         newDir match
             case ProjectTemplateLocalPath(path) =>
                 dirs.map((dir) => os.copy.into(dir.path, path))
